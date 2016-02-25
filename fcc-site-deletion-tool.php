42a161001edde01565db813174442fa4a34a139d
Ryan Veitch <ryan.r.veitch@gmail.com>
2016-02-25 17:45:11 -0600

    Added files via upload

---
+++ fcc-site-deletion-tool.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: FCC Site Deletion Tool
+ * Plugin URI:  http://www.forumcomm.com/
+ * Author:      Forum Communications Company
+ * Author URI:  http://www.forumcomm.com/
+ * Version:     0.0.1
+ * Description: Starter template for FCC WordPress Plugins.
+ * License:     GPL v2 or later
+ * Text Domain: fcc-plugin-template
+ */
+
+# Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
+
+/*--------------------------------------------------------------
+# SECTION NAME
+--------------------------------------------------------------*/
