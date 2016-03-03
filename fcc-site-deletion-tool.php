a2bd4346a71b826bdb87262ffbcd58658525860c
ryan.veitch <ryan.r.veitch@gmail.com>
2016-03-02 18:31:37 -0600

    Added 'Splogs' JSON Feed
    Feed URL: example.com?feed=splogs

--- fcc-site-deletion-tool.php
+++ fcc-site-deletion-tool.php
@@ -4,8 +4,8 @@
  * Plugin URI:  http://www.forumcomm.com/
  * Author:      Forum Communications Company
  * Author URI:  http://www.forumcomm.com/
- * Version:     0.0.1
- * Description: Starter template for FCC WordPress Plugins.
+ * Version:     0.16.03.02
+ * Description: WP-CLI site deletion tool. Feed URL: example.com?feed=splogs
  * License:     GPL v2 or later
  * Text Domain: fcc-plugin-template
  */
@@ -14,5 +14,46 @@
 defined( 'ABSPATH' ) || exit;
 
 /*--------------------------------------------------------------
-# SECTION NAME
+# PLUGIN ACTIVATION/DEACTIVATION HOOKS
 --------------------------------------------------------------*/
+
+ /**
+  * Plugin Activation Hook
+  */
+ function fcc_site_deletion_tool_activation() {
+   # Flush our rewrite rules on activation.
+   flush_rewrite_rules();
+ }
+ register_activation_hook( __FILE__, 'fcc_site_deletion_tool_activation' );
+
+ /**
+  * Plugin Deactivation Hook
+  */
+ function fcc_site_deletion_tool_deactivation() {
+   # Flush our rewrite rules on deactivation.
+   flush_rewrite_rules();
+ }
+ register_deactivation_hook( __FILE__, 'fcc_site_deletion_tool_deactivation' );
+
+ /*--------------------------------------------------------------
+ # JSON FEED
+ --------------------------------------------------------------*/
+
+ /**
+  * Add 'splogs' JSON Feed
+  *
+  * @since 0.16.03.02
+  */
+ function fcc_splogs_do_json_feed(){
+   add_feed('splogs', 'add_splogs_feed');
+ }
+ add_action('init', 'fcc_splogs_do_json_feed');
+
+ /**
+  * Load JSON Feed Template
+  *
+  * @since 0.16.03.02
+  */
+ function add_splogs_feed(){
+	load_template( plugin_dir_path( __FILE__ ) . 'template/feed-json.php' );
+ }
---
+++ template/feed-json.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * JSON Feed Template
+ *
+ * @since 0.16.03.02
+ */
+$callback = trim(esc_html(get_query_var('callback')));
+$charset  = get_option('charset');
+
+$deletion_ids = get_option('deletion_ids');
+$deletion_ids = explode( ',', $deletion_ids );
+
+if ( $deletion_ids ) {
+
+	$json = $deletion_ids;
+
+	$json = json_encode($json);
+
+	nocache_headers();
+	if (!empty($callback)) {
+		header("Content-Type: application/x-javascript; charset={$charset}");
+		echo "{$callback}({$json});";
+	} else {
+		header("Content-Type: application/json; charset={$charset}");
+		echo $json;
+	}
+
+} else {
+	status_header('404');
+	wp_die("404 Not Found");
+}
